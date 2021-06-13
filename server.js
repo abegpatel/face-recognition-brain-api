8f1c504310828efe1c94f38e1fd213fb91c5244b
abegpatel <abegpatel@gmail.com>
2021-06-13 23:53:42 +0530

    port

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
- 
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
